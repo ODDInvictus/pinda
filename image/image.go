908d2a197ae46e45066aa17e458548a3c811afba
Niels <56843328+Nierot@users.noreply.github.com>
2024-12-04 23:45:50 +0100

    chore: add image stub

--- image/image.go
+++ image/image.go
@@ -1,1 +1,17 @@
 package image
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func Init(rdb *redis.Client) {
+	go compressImage(rdb)
+}
+
+func compressImage(rdb *redis.Client) {
+	ctx := context.Background()
+
+	rdb.Subscribe(ctx, "compress-image")
+}
